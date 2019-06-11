6565120ffd9483a4b354fa00d3e08daa7ba9b2ea
DamColson <damien.colson@gmail.com>
2019-06-11 16:46:07 +0200

    maj oubliée add --a

--- error_404.php
+++ error_404.php
@@ -14,7 +14,7 @@
 
         <h1>Erreur 404</h1>
         <h3>La page recherchée est inexistante</h3>
-        <h5>Cette page est personnalisée !! que tu le veuilles ou non ANOUSONE !!! <h5>
+        <h5>Cette page est personnalisée !! Que tu le veuilles ou non ANOUSONE !!! <h5>
 
         <script src = "https://code.jquery.com/jquery-3.4.0.js" integrity = "sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin = "anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
