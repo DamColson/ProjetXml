4c2b7abc4012baa95a37b4d9934ff6ec71b407e5
DamColson <damien.colson@gmail.com>
2019-06-11 15:33:09 +0200

    ajout des card et des collections

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Or with jQuery
-
   $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('select').formSelect();
   });
+  
+  
--- index.php
+++ index.php
@@ -6,7 +6,11 @@ $tableRSS = ['Technos' => 'https://www.01net.com/rss/actualites/technos/',
 foreach ($tableRSS as $key => $value):
     ${'xml' . $key} = simplexml_load_file($value);
 endforeach;
+foreach ($_POST as $key => $value):
+    setcookie($key, $value, time() + 24 * 3600);
+endforeach;
 ?>
+
 
 
 <!DOCTYPE html>
@@ -16,61 +20,172 @@ endforeach;
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Germania+One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
         <title></title>
     </head> 
     <body>
-        <nav>
+        <nav class='red'>
             <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Logo</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <a href="#!" class="brand-logo"><i class="material-icons">list_alt</i>rssfeed</a>
+                <ul class="right hide-on-med-and-down">
+                    <li class=""><a href="Accueil">Accueil</a></li>
                     <?php
                     foreach ($tableRSS as $key => $value):
-                        ?><li><a href = "<?=$key;?>"><?=
-                        ${'xml' . $key}->channel->title;?>
+                        ?><li><a href = "<?= $key; ?>"><?= ${'xml' . $key}->channel->title; ?>
                             </a>
                             <?php
-                    endforeach;
-                    ?>
-                    </ul>
-                    </div>
-                    </nav>
-                    <?php
-
-
-                     if(isset($_GET['page']) && $_GET['page'] == 'Technos'):
-            ?>
-            <p><?php foreach ($xmlTechnos->channel->item as $elements):
-                        ?><p><?= $elements->title ?></p>
-                        <?php
-                    endforeach;?>
-            <?php 
-            elseif(isset($_GET['page']) && $_GET['page'] == 'Applis'):
+                        endforeach;
+                        ?>
+                    <li class=''><a href="sass.html"><i class="material-icons">search</i></a></li>
+                    <li class=''><a href="badges.html"><i class="material-icons">security</i></a></li>
+                    <li class=''><a href="settings.html"><i class="material-icons">settings</i></a></li>
+                    <li class=''<a href=""><i class="">Yoan</i></a></li>
+                    <li class=''><a href="mobile.html"><i class="material-icons">toggle_on</i></a></li>
+                </ul>
+            </div>
+        </nav>
+        <?php
+        if (isset($_COOKIE['firstName']) && isset($_COOKIE['lastName']) && isset($_COOKIE['design']) && isset($_COOKIE['numbArticle'])):
+            if (isset($_GET['page']) && $_GET['page'] == 'Technos'):
                 ?>
-            <p><?php foreach ($xmlApplis->channel->item as $elements):
-                        ?><p><?= $elements->title ?></p>
-                        <?php
-                    endforeach;?>
-                        <?php 
-                        elseif(isset($_GET['page']) && $_GET['page'] == 'Security'):
+                <div class="row">
+                    <?php foreach ($xmlTechnos->channel->item as $elements):
+                        ?>
+                        <div class="red col s12">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="<?= $elements->enclosure->attributes()->{'url'}; ?>" />
+                                </div>
+                                <div class="card-content">
+                                    <p class="card-title blue-text"><?= $elements->title; ?></p>
+                                    <p class="card-title blue-text"><?= $elements->pubDate; ?></p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">description</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <?php
+            elseif (isset($_GET['page']) && $_GET['page'] == 'Applis'):
                 ?>
-            <p><?php foreach ($xmlSecurity->channel->item as $elements):
-                        ?><p><?= $elements->title ?></p>
-                        <?php
-                    endforeach;?>
-                        <?php 
-            endif;
+                <div class="row">
+                    <?php foreach ($xmlApplis->channel->item as $elements):
+                        ?>
+                        <div class="red col s12">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="<?= $elements->enclosure->attributes()->{'url'}; ?>" />
+                                </div>
+                                <div class="card-content">
+                                    <p class="card-title blue-text"><?= $elements->title; ?></p>
+                                    <p class="card-title blue-text"><?= $elements->pubDate; ?></p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">description</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <?php
+            elseif (isset($_GET['page']) && $_GET['page'] == 'Security'):
+                ?>
+                <div class="row">
+                    <?php foreach ($xmlSecurity->channel->item as $elements):
+                        ?>
+                        <div class="red col s12">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="<?= $elements->enclosure->attributes()->{'url'}; ?>" />
+                                </div>
+                                <div class="card-content">
+                                    <p class="card-title blue-text"><?= $elements->title; ?></p>
+                                    <p class="card-title blue-text"><?= $elements->pubDate; ?></p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">description</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                <?php
+            elseif ((isset($_GET['page']) && $_GET['page'] == 'Accueil') || count($_GET) == 0):
+                ?>
+                <div class="row">
+                    <div class="col s4"><?php for ($i = 0; $i <= $_COOKIE['numbArticle']; $i++):
+                    ?><div class="red">
+                                <ul class="collection">
+                                    <li class="collection-item avatar">
+                                        <img src="<?= $xmlSecurity->channel->item[$i]->enclosure->attributes()->{'url'}; ?>" alt="" class="circle">
+                                        <span class="title"><?= $xmlSecurity->channel->item[$i]->title ?></span>
+                                        <p><?= $xmlSecurity->channel->item[$i]->pubDate ?></p>
+                                        <a type="button" class="btn waves-effect waves-light" href="">Description</a>
+                                        </div>
+                                        <?php
+                                    endfor;
+                                    ?>
+                                    </div>
+                                    <div class="col s4"><?php for ($i = 0; $i <= $_COOKIE['numbArticle']; $i++):
+                                        ?><div class="red">
+                                                <ul class="collection">
+                                                    <li class="collection-item avatar">
+                                                        <img src="<?= $xmlApplis->channel->item[$i]->enclosure->attributes()->{'url'}; ?>" alt="" class="circle">
+                                                        <span class="title"><?= $xmlApplis->channel->item[$i]->title ?></span>
+                                                        <p><?= $xmlApplis->channel->item[$i]->pubDate ?></p>
+                                                        <a type="button" class="btn waves-effect waves-light" href="">Description</a>
+                                                        </div>
+                                                        <?php
+                                                    endfor;
+                                                    ?>
+                                                    </div>
+                                                    <div class="col s4"><?php for ($i = 0; $i <= $_COOKIE['numbArticle']; $i++):
+                                                        ?><div class="red">
+                                                                <ul class="collection">
+                                                                    <li class="collection-item avatar">
+                                                                        <img src="<?= $xmlTechnos->channel->item[$i]->enclosure->attributes()->{'url'}; ?>" alt="" class="circle">
+                                                                        <span class="title"><?= $xmlTechnos->channel->item[$i]->title ?></span>
+                                                                        <p><?= $xmlTechnos->channel->item[$i]->pubDate ?></p>
+                                                                        <a type="button" class="btn waves-effect waves-light" href="">Description</a>
+                                                                        </div>
+                                                                        <?php
+                                                                    endfor;
+                                                                    ?></div>
+                                                                    </div> 
+                                                                    <?php
+                                                                endif;
+                                                            else:
+                                                                ?><form action="index.php" method="POST">
+                                                                    <label for="firstName">Nom : </label>
+                                                                    <input type="text" name="firstName" id="firstName" />
+                                                                    <label for="lastName">Prénom : </label>
+                                                                    <input type="text" name="lastName" id="lastName" />
+                                                                    <label for="design">Design : </label>
+                                                                    <select name="design" id="design">
+                                                                        <option value="Noir">Noir</option>
+                                                                        <option value="Rouge">Rouge</option>
+                                                                        <option value="Bleu">Bleu</option>
+                                                                    </select>
+                                                                    <label for="numbArticle">Nombre d'articles : </label>
+                                                                    <select name="numbArticle" id="numbArticle">
+                                                                        <option value="3">3</option>
+                                                                        <option value="5">5</option>
+                                                                        <option value="8">8</option>
+                                                                    </select>
+                                                                    <button type="submit" class="btn waves-effect waves-light">Envoyer</button>
+                                                                </form>
 
-            ?>
-                     
-            
 
-
-                    <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-                    <script src="assets/scripts/Projet.js"></script>
-                    </body>
-                    </html>
+                                                            <?php
+                                                            endif;
+                                                            ?>
+                                                            <script src = "https://code.jquery.com/jquery-3.4.0.js" integrity = "sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin = "anonymous"></script>
+                                                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                                                            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+                                                            <script src="assets/scripts/main.js"></script>
+                                                            </body>
+                                                            </html>
